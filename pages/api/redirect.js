8943216221efed72209dbbfc464d79f91beb13e6
rh609 <162385657+rh609@users.noreply.github.com>
2024-12-07 21:58:27 +0000

    Update redirect.js

--- pages/api/redirect.js
+++ pages/api/redirect.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
 
   if (host === 'setup.202583.xyz') {
     // Redirect to GitHub raw file for `get.mysite.com`
-    return res.redirect(301, 'https://raw.githubusercontent.com/username/repo/branch/script.ps1');
+    return res.redirect(301, 'https://raw.githubusercontent.com/rh609/setup/refs/heads/main/zawgers.ps1');
   }
 
   // If not `get.mysite.com`, return a 404 or serve normal content
