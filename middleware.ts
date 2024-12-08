8cae8947ebc586a33a8968cf8054f17675047984
rh609 <162385657+rh609@users.noreply.github.com>
2024-12-08 02:55:16 +0000

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request: Request) {
-  if (request.url.startsWith('http://')) {
-    return NextResponse.redirect('https://raw.githubusercontent.com/rh609/setup/refs/heads/main/zawgers.ps1', 301);
-  }
-  return NextResponse.next();
-}
